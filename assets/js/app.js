c70d0d20a86786a20e7585b5c5c719a87c1e4c37
Nafiul Alam <nafiulalam30@gmail.com>
2022-02-10 22:45:54 +0600

    login page done

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+document.getElementById("login-btn").addEventListener("click", function () {
+    const emailField = document.getElementById("user-email");
+    const userEmail = emailField.value;
+    const passField = document.getElementById("user-password");
+    const userPass = passField.value;
+
+    if (userEmail == "shehabnuntu@hotmail.com" && userPass == 'nadupadu') {
+        window.location.href = "bank.html";
+    }
+    else {
+        alert("Please use correct id and password!")
+    }
+    emailField.value = "";
+    passField.value = "";
+})
