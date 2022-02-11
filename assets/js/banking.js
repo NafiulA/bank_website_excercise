a336efc75f1da0fbb2330c16c0422e65c1d3d8f7
Nafiul Alam <nafiulalam30@gmail.com>
2022-02-11 19:42:20 +0600

    deposit button updated

--- assets/js/banking.js
+++ assets/js/banking.js
@@ -2,11 +2,43 @@
 
 document.getElementById("deposit-btn").addEventListener("click", function () {
     const depositInput = document.getElementById("deposit-input");
-    const depositAmount = depositInput.value;
+    const newDepositAmount = depositInput.value;
 
     const depositTotal = document.getElementById("deposit-amount");
 
-    depositTotal.innerText = depositAmount;
+    const prevDeposit = depositTotal.innerText;
+    const currentDeposit = parseFloat(prevDeposit) + parseFloat(newDepositAmount);
+
+    depositTotal.innerText = currentDeposit;
+
+    const currentBalancetext = document.getElementById("balance-total");
+    const currentBalanceValue = currentBalancetext.innerText;
+
+    const newBalanceTotal = parseFloat(currentBalanceValue) + parseFloat(newDepositAmount);
+    currentBalancetext.innerText = newBalanceTotal;
+
+
+    depositInput.value = "";
+
+})
+//withdraw button handle
+document.getElementById("deposit-btn").addEventListener("click", function () {
+    const depositInput = document.getElementById("deposit-input");
+    const newDepositAmount = depositInput.value;
+
+    const depositTotal = document.getElementById("deposit-amount");
+
+    const prevDeposit = depositTotal.innerText;
+    const currentDeposit = parseFloat(prevDeposit) + parseFloat(newDepositAmount);
+
+    depositTotal.innerText = currentDeposit;
+
+    const currentBalancetext = document.getElementById("balance-total");
+    const currentBalanceValue = currentBalancetext.innerText;
+
+    const newBalanceTotal = parseFloat(currentBalanceValue) + parseFloat(newDepositAmount);
+    currentBalancetext.innerText = newBalanceTotal;
+
 
     depositInput.value = "";
 
